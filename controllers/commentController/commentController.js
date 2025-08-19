fbab08dd9deae55e5350fe72b4e39189fa60a352
Tanvirgit07 <ahmmedtanvir9090@gmail.com>
2025-08-19 15:31:38 +0600

    new add

--- controllers/commentController/commentController.js
+++ controllers/commentController/commentController.js
@@ -51,7 +51,7 @@ const createComment = async (req, res, next) => {
 const editComment = async (req, res, next) => {
   try {
     const { id } = req.params;
-    console.log(productId,userId,commentText,id)
+    console.log(productId, userId, commentText, id);
 
     if (!id || !productId || !userId) {
       return res.status(400).json({
@@ -94,8 +94,114 @@ const editComment = async (req, res, next) => {
       message: "Comment edit successfully ",
     });
   } catch (err) {
-    next(handleError(500, err.message))
+    next(handleError(500, err.message));
+  }
+};
+const getALlComment = async (req, res, next) => {
+  try {
+    const allcomment = await CommentModel.find();
+    res.status(200).json({
+      success: true,
+      message: "Fatch All Comment Successfully",
+      data: allcomment,
+    });
+  } catch (err) {
+    next(handleError(500, err.message));
+  }
+};
+const getSingleComment = async (req, res, next) => {
+  try {
+    const { productId, userId, id } = req.params;
+    console.log(productId, userId, id);
+    const existingComment = await CommentModel.findById(id);
+    if (!existingComment) {
+      return res.status(400).json({
+        success: false,
+        message: "Comment not found",
+      });
+    }
+
+    console.log(existingComment.productId, productId);
+
+    if (existingComment.productId.toString() !== productId) {
+      return res.status(400).json({
+        success: false,
+        message: "Product Id invalid",
+      });
+    }
+
+    if (existingComment.userId.toString() !== userId) {
+      return res.status(400).json({
+        success: false,
+        message: "User Id invalid",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Fatch Single comment successfully",
+      data: existingComment,
+    });
+  } catch (err) {
+    next(handleError(500, err.message));
+  }
+};
+const deleteComment = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const existingComment = await CommentModel.findById(id);
+
+    if (!existingComment) {
+      return res.status(400).json({
+        success: false,
+        message: "Comment not found",
+      });
+    }
+
+    const existingCommentUser = await UserModel.findOne({
+      _id: existingComment.userId,
+    });
+    if (!existingCommentUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    existingCommentUser.comments = existingCommentUser.comments.filter(
+      (UCID) => UCID !== existingComment._id
+    );
+    await existingCommentUser.save();
+
+    const existingCommentFood = await FoodModel.findOne({
+      _id: existingComment.productId,
+    });
+    if (!existingCommentFood) {
+      return res.status(400).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    existingCommentFood.comments = existingCommentFood.comments.filter(
+      (FCID) => FCID !== existingComment._id
+    );
+    await existingCommentFood.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Comment delete successfully",
+      data: existingComment,
+    });
+  } catch (err) {
+    next(handleError(500, err.message));
   }
 };
 
-module.exports = { createComment, editComment };
+module.exports = {
+  createComment,
+  editComment,
+  getALlComment,
+  getSingleComment,
+  deleteComment
+};
--- routers/commentRouter/commentRouter.js
+++ routers/commentRouter/commentRouter.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { createComment, editComment } = require('../../controllers/commentController/commentController');
+const { createComment, editComment, getALlComment, getSingleComment, deleteComment } = require('../../controllers/commentController/commentController');
 const commentRouter = express.Router();
 
 
 commentRouter.post('/createcomment',createComment);
 commentRouter.post('/editcomment/:id',editComment);
+commentRouter.get('/allcomment', getALlComment);
+commentRouter.get('/singlecomment/:productId/:userId/:id', getSingleComment);
+commentRouter.delete('/deletecomment/:id', deleteComment);
 
 
 module.exports = commentRouter
