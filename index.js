51de9c73bec20019c96fc34bdd4c6c16b5c7ae47
Tanvirgit07 <ahmmedtanvir009@gmail.com>
2025-11-17 01:48:19 +0600

    add

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ app.use("/api/v1",webHookRouter)
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003","http://localhost:3004"],
-    // credentials: true,
+    origin: "*",
   })
 );
+
 
 
 
